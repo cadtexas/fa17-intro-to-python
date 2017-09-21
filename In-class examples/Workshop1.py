8b5aefa7276a5a99839fa83cb576a3f64e4ff26f
edpriego <epriego@me.com>
2017-09-21 08:24:59 -0500

    Add files via upload

---
+++ In-class examples/Workshop1.py
@@ -0,0 +1,38 @@
+# print statement
+# computer does not read whatever comes
+# after pound signs
+
+'''
+print("Hello World")
+print()
+print("This is the first class")
+print("")
+
+Everything in between these single quotes
+will not be read by computer
+'''
+print("print this")
+
+print('this')
+
+print(3+2)
+print("3+2")
+print("3"+"2")
+#print("this string"+"another string")
+# boolean (data type) = True or False
+
+a='5'
+b = 5
+#print(a+b+"python")
+
+#boolean
+
+c = True
+d = False
+
+'''
+== comparison
+= setting a value
+'''
+print(a==b)
+print(c==d)
