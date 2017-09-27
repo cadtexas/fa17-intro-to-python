2f2881855e50f4db1e7695e79623247aba8db4c5
Eduardo Priego <epriego@me.com>
2017-09-27 18:41:06 -0500

    In-class exercises

---
+++ In-class exercises/ExercisesWeek2.py
@@ -0,0 +1,59 @@
+# Here are a few exercises to work on
+# Make sure to un-comment each exercise by deleting the quotes (''')
+
+'''
+# Exercise 1
+# Simultaneous assignments
+
+# Prompt the user to enter three numbers
+number1, number2, number3 = ???
+
+# Compute average
+average = (number1 + number2 + number3) / 3
+
+# Display result
+print("The average of", number1, number2, number3, "is", average)
+'''
+
+'''
+# Exercise 2
+# You can import the math module and use various functions
+
+import math #imports the module
+
+print(math.pi)
+print(math.cos(math.pi))
+
+# Now print out what 1.57 radians is in degrees (the function is degrees())
+print()
+# Print the square root of 2025 (sqrt())
+print()
+# What about the log of 80? (log(x, base))
+'''
+
+'''
+# Exercise 3
+# You can print something with a specific ending and it won't automatically go to the next line
+
+print("Python", end=" is cool")
+print("This will be on the same line")
+
+# Now make it so that the following is all on the same line and separated by an asterisk 
+# Like this: This prints*out without*a new line
+print("This prints")
+print("out without")
+print("a new line")
+'''
+
+'''
+# Exercise 4
+# Ask user to enter a weight in kilograms and convert it to pounds
+
+weightKG = ??
+
+# conversion (1 lb = 0.454 kg)
+weightLBS = ?
+
+print("The weight in pounds is: ", weightLBS)
+'''
+
